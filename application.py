d7f6a1d04d66d789c28255b98dad95658feaa582
william1997 <ivanisaacgarcia10@gmail.com>
2015-09-30 10:49:38 -0600

    I added a desing to the poyect

--- application.py
+++ application.py
@@ -1,16 +1,19 @@
+from datetime import *
 import sys
 import os
 from decimal import Decimal
 article = {}
-print "WELCOME TO THE 'CHOCO CHAVO'S' STORE"
+print"==============================================================================================="
+print "\t\t\t\tWELCOME TO THE 'CHOCO CHAVO'S' STORE"
+print"==============================================================================================="
 def menu():
     print """
-      please select an option:
+        please select an option:
       
         1. Add item
         2. Sell articles
         3.Exit/Quit\n"""
-    selection =raw_input("enter your option: ")
+    selection =raw_input("       enter your option: ")
     if selection == "1":
         os.system("clear")
         Add()
@@ -19,7 +22,9 @@ def menu():
         Sell()
     elif selection == "3":
         os.system("clear") 
-        print"\nGood bye, thank you for buy with us\n"
+        print"\n*********Good bye, thank you for buy with us******"
+        print "  ****************Comeback soon*****************"
+        print "    ******P.S. CHOCO CHAVO'S STORE*****"
         sys.exit()
     else: 
         print "Enter a number from 1 to 3"
@@ -37,9 +42,10 @@ def Add():
     while price == "":
         try:
             price = float(raw_input("Enter the price: "))
-            print item, price
+            print "_______ARTICLE____PRICE____________"
+            print "       ",item,"   ",price
+            print "___________________________________"
             article[item] = price
-            print article
             print "Your item was added"
         except ValueError :
             print "You need enter a value"
@@ -65,8 +71,10 @@ def Sell():
         Add()
     else: 
         for i in article:
+            print"______ARTICLES_THAT_EXISTS_____"
             print "article: ", i 
             print "price: ", article[i]
+            print "______________________________"
         article2 = []
         No_total = 0
         while True :
@@ -74,15 +82,22 @@ def Sell():
 and if you want go to the bill enter DONE"""
             item_sell= raw_input("enter the item you want to sell: ")
             article2.append(item_sell)
-            print article2
+            os.system("clear")
+            print "you have in your car shop"
+            print " ".join(article2)
             if item_sell == "done":
                 item_sell = "done"
             else: 
                 card = cards(item_sell, Discount,article2)
             if item_sell == "done" or item_sell == "DONE" or item_sell == "Done":
-                print "================================="
-                print "item", " |  price"  ," | quantity"
-                print "================================="
+                os.system("clear")
+                print "\t\t\t CHOCO CHAVO'S STORE"
+                print "   today :",datetime.today()
+                name =  raw_input("   client name: ")
+                nit = raw_input("   client NIT: ")
+                print "========================================="
+                print "item", "   |    price", " |    quantity"
+                print "=========================================" 
                 for i in article:
                     repeat = article2.count(i) 
                     sell = article[i] * repeat
@@ -94,18 +109,26 @@ and if you want go to the bill enter DONE"""
                     total_card = card * total_iva 
                     total = total_iva - total_card 
                     if repeat >= 1:
-                        print i,"   ","%.2f" % (article[i]),"     *",repeat
+                        print i,"      ","%.2f" % (article[i]),"  *    ",repeat
+                print "-------------------------------------------"
                 print "the IVA is: " + "%.2f" % (iva)
+                print "-------------------------------------------"
                 print "the total price whithout IVA is: $" "%.2f" % (No_total)
+                print "-------------------------------------------"
                 print "the total price whith IVA is: $" + "%.2f" % (total_iva)
-                print card
+                print "-------------------------------------------"
                 if card == 0: 
                     print "You don't have a card"
-                else: 
+                else:
+                    print "your descount with your card is %",card 
                     print "The total with card descount is: $" + "%.2f" % (total)
+                    print "-----------------------------------------"
                 pay = float(raw_input("pay with: "))
                 change = pay - total
                 print "the change is: $" + "%.2f" % (change)
+                print"\n*********Good bye, thank you for buy with us******"
+                print "  ***************Comeback soon*****************"
+                print "     ******P.S. CHOCO CHAVO'S STORE*****"
                 menu() 
 def cards(item_sell, Discount,article2):
     for i in article2: 
